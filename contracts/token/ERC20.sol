393cebe3d68983dc6f33bdae6d665cf090e02609
x1n13y84issmd42 <x1n13y84issmd42@gmail.com>
2026-04-02 17:15:47 +0300

    Added ERC20 token

---
+++ contracts/token/ERC20.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.28;
+
+contract Owned {
+	address payable public owner;
+
+	error OnlyOwnerAllowed();
+
+	modifier OnlyOwner {
+		if (msg.sender != owner) {
+			revert OnlyOwnerAllowed();
+		}
+
+		_;
+	}
+
+	constructor() {
+		owner = payable(msg.sender);
+	}
+}
+
+contract ERC20Token is Owned {
+	mapping (address => uint256) public balanceOf;
+	mapping (address => mapping (address => uint256)) public allowance;
+
+	string public name;
+	string public symbol;
+	uint256 public totalSupply;
+	uint8 public decimals;
+	
+	event Approval(address indexed owner, address indexed spender, uint256 amount);
+	event Transfer(address indexed from, address indexed to, uint256 amount);
+
+	error InsufficientFunds(address spender, uint256 funds, uint256 amount);
+	error InsufficientAllowance(address owner, address spender, uint256 allowance, uint256 amount);
+	error InvalidSender(address a);
+	error InvalidReceiver(address a);
+
+	constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+		name = _name;
+		symbol = _symbol;
+		decimals = _decimals;
+	}
+
+	function mint(address to, uint256 amount) external OnlyOwner {
+		if (to == address(0)) {
+			revert InvalidReceiver(address(0));
+		}
+
+		balanceOf[to] += amount;
+		totalSupply += amount;
+	}
+
+	function transfer(address to, uint256 amount) external returns(bool) {
+		_validateAddresses(msg.sender, to);
+		return _transfer(msg.sender, to, amount);
+	}
+
+	function approve(address spender, uint256 amount) external returns(bool) {
+		if (spender == address(0)) {
+			revert InvalidReceiver(address(0));
+		}
+
+		allowance[msg.sender][spender] = amount;
+
+		emit Approval(msg.sender, spender, amount);
+
+		return true;
+	}
+
+	function transferFrom(address from, address to, uint256 amount) external returns(bool) {
+		_validateAddresses(from, to);
+
+		if (msg.sender != from) {
+			uint256 a = allowance[from][msg.sender];
+
+			if (a < amount) {
+				revert InsufficientAllowance(from, msg.sender, a, amount);
+			}
+
+			unchecked {
+				allowance[from][msg.sender] = a - amount;
+			}
+		}
+
+		return _transfer(from, to, amount);
+	}
+
+	function _transfer(address from, address to, uint256 amount) internal returns(bool){
+		if (balanceOf[from] < amount) {
+			revert InsufficientFunds(from, balanceOf[from], amount);
+		}
+
+		unchecked {
+			balanceOf[from] -= amount;
+			balanceOf[to] += amount;	
+		}
+
+		emit Transfer(from, to, amount);
+
+		return true;
+	}
+
+	function _validateAddresses(address from, address to) internal pure {
+		if (from == address(0)) {
+			revert InvalidSender(address(0));
+		}
+
+		if (to == address(0)) {
+			revert InvalidReceiver(address(0));
+		}
+	}
+}
---
+++ test/ERC20.ts
@@ -0,0 +1,294 @@
+import { network } from 'hardhat';
+import { describe, it, afterEach } from 'node:test';
+import assert from 'node:assert';
+import * as testutils from '../../utility/testutils.js'
+import { getAddress } from 'viem';
+
+describe("ERC20Token", async function () {
+	const { viem, networkHelpers } = await network.connect();
+	const client = await viem.getPublicClient();
+	
+	const clients = await viem.getWalletClients();
+	const contractOwner = clients[0];
+	const coinOwner1 = clients[1];
+	const coinOwner2 = clients[2];
+	const coinSpender = clients[3];
+	const coinReceiver = clients[4];
+
+	const cERC20Token = await viem.deployContract("ERC20Token", ["TestCoin", "XTC", 8], {client: {wallet: contractOwner}});
+
+	const addr0 = "0x0000000000000000000000000000000000000000";
+
+	afterEach(async function () {
+		// So every case starts with a new block.
+		// This is needed to count events.
+		networkHelpers.mine();
+	});
+
+	it("metadata", async function () {
+		const name = await cERC20Token.read.name();
+		const symbol = await cERC20Token.read.symbol();
+		const decimals = await cERC20Token.read.decimals();
+
+		assert.equal(name, "TestCoin");
+		assert.equal(symbol, "XTC");
+		assert.equal(decimals, 8);
+	});
+
+	it("mint()", async function () {
+		const amount1 = 123n;
+		const amount2 = 45n;
+		await cERC20Token.write.mint([coinOwner1.account.address, amount1], {account: contractOwner.account});
+		await cERC20Token.write.mint([coinOwner2.account.address, amount2], {account: contractOwner.account});
+
+		const coinOwner1Balance = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const coinOwner2Balance = await cERC20Token.read.balanceOf([coinOwner2.account.address]);
+		const totalSupply = await cERC20Token.read.totalSupply();
+
+		assert.equal(coinOwner1Balance, amount1);
+		assert.equal(coinOwner2Balance, amount2);
+		assert.equal(totalSupply, amount1 + amount2);
+	});
+
+	it("mint() - revert OnlyOwnerAllowed", async function () {
+		try {
+			await cERC20Token.write.mint([coinOwner1.account.address, 121n], {account: coinOwner1.account});
+			assert.fail("Contract chould have reverted.");
+		} catch(e: any) {
+			assert.equal(
+				e.details,
+				testutils.contractErrorMessage('OnlyOwnerAllowed')
+			);
+		}
+	});
+
+	it("mint() to address(0) - revert InvalidReceiver", async function () {
+		try {
+			await cERC20Token.write.mint([addr0, 1n], {account: contractOwner.account});
+			assert.fail("Contract chould have reverted.");
+		} catch(e: any) {
+			assert.equal(
+				e.details,
+				testutils.contractErrorMessage('InvalidReceiver', addr0)
+			);
+		}
+	});
+
+	it("approve()", async function () {
+		const blockN = await client.getBlockNumber();
+
+		const allowance1 = await cERC20Token.read.allowance([coinOwner1.account.address, coinSpender.account.address]);
+		assert.equal(allowance1, 0);
+		
+		const approvedAmount = 12n;
+		await cERC20Token.write.approve([coinSpender.account.address, approvedAmount], {account: coinOwner1.account});
+
+		const allowance2 = await cERC20Token.read.allowance([coinOwner1.account.address, coinSpender.account.address]);
+		assert.equal(allowance2, approvedAmount);
+
+		const events = await client.getContractEvents({
+			abi: cERC20Token.abi,
+			eventName: 'Approval',
+			fromBlock: blockN
+		});
+
+		assert.equal(events.length, 1);
+		assert.deepEqual(events[0].args, {
+			owner: getAddress(coinOwner1.account.address),
+			spender: getAddress(coinSpender.account.address),
+			amount: approvedAmount,
+		});
+	});
+
+	it("approve() for address(0) - revert InvalidReceiver", async function () {
+		try {
+			await cERC20Token.write.approve([addr0, 1n], {account: contractOwner.account});
+			assert.fail("Contract chould have reverted.");
+		} catch(e: any) {
+			assert.equal(
+				e.details,
+				testutils.contractErrorMessage('InvalidReceiver', addr0)
+			);
+		}
+	});
+
+	it("transfer()", async function () {
+		const amount = 5n;
+		const blockN = await client.getBlockNumber();
+
+		const balanceOwner1 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver1 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+		
+		await cERC20Token.write.transfer([coinReceiver.account.address, amount], {account: coinOwner1.account});
+
+		const balanceOwner2 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver2 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+
+		assert.equal(balanceOwner2, balanceOwner1 - amount);
+		assert.equal(balanceReceiver2, balanceReceiver1 + amount);
+
+		const events = await client.getContractEvents({
+			abi: cERC20Token.abi,
+			eventName: 'Transfer',
+			fromBlock: blockN,
+		});
+
+		assert.equal(events.length, 1);
+		assert.deepEqual(events[0].args, {
+			from: getAddress(coinOwner1.account.address),
+			to: getAddress(coinReceiver.account.address),
+			amount,
+		});
+	});
+
+	it("transfer() over 9000 - revert InsufficientFunds", async function () {
+		const amount = 9001n;
+		const blockN = await client.getBlockNumber();
+
+		const balanceOwner1 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver1 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+
+		try {
+			await cERC20Token.write.transfer([coinReceiver.account.address, amount], {account: coinOwner1.account});
+			assert.fail("Contract chould have reverted.");
+		} catch(e: any) {
+			assert.equal(
+				e.details,
+				testutils.contractErrorMessage(
+					'InsufficientFunds',
+					coinOwner1.account.address,
+					balanceOwner1,
+					amount,
+				)
+			);
+		}
+
+		const balanceOwner2 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver2 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+
+		// Balances should stay unchanged.
+		assert.equal(balanceOwner2, balanceOwner1);
+		assert.equal(balanceReceiver2, balanceReceiver1);
+
+		const events = await client.getContractEvents({
+			abi: cERC20Token.abi,
+			eventName: 'Transfer',
+			fromBlock: blockN,
+		});
+
+		// No events should have fired.
+		assert.equal(events.length, 0);
+	});
+
+	it("transfer() to address(0) - revert InvalidReceiver", async function () {
+		try {
+			await cERC20Token.write.transfer([addr0, 1n], {account: coinOwner1.account});
+			assert.fail("Contract chould have reverted.");
+		} catch (e: any) {
+			assert.equal(e.details, testutils.contractErrorMessage('InvalidReceiver', addr0));
+		}
+	});
+
+	it("transferFrom() coin owner", async function () {
+		const amount = 2n;
+		const blockN = await client.getBlockNumber();
+
+		const balanceOwner1 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver1 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+		
+		await cERC20Token.write.transferFrom([coinOwner1.account.address, coinReceiver.account.address, amount], {account: coinOwner1.account});
+
+		const balanceOwner2 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver2 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+
+		assert.equal(balanceOwner2, balanceOwner1 - amount);
+		assert.equal(balanceReceiver2, balanceReceiver1 + amount);
+
+		const events = await client.getContractEvents({
+			abi: cERC20Token.abi,
+			eventName: 'Transfer',
+			fromBlock: blockN,
+		});
+
+		assert.equal(events.length, 1);
+		assert.deepEqual(events[0].args, {
+			from: getAddress(coinOwner1.account.address),
+			to: getAddress(coinReceiver.account.address),
+			amount,
+		});
+	});
+
+	it("transferFrom() approved spender", async function () {
+		const amount = 2n;
+		const blockN = await client.getBlockNumber();
+
+		const balanceOwner1 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver1 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+		const allowanceSpender1 = await cERC20Token.read.allowance([coinOwner1.account.address, coinSpender.account.address]);
+		
+		await cERC20Token.write.transferFrom(
+			[coinOwner1.account.address, coinReceiver.account.address, amount],
+			{account: coinSpender.account},
+		);
+
+		const balanceOwner2 = await cERC20Token.read.balanceOf([coinOwner1.account.address]);
+		const balanceReceiver2 = await cERC20Token.read.balanceOf([coinReceiver.account.address]);
+		const allowanceSpender2 = await cERC20Token.read.allowance([coinOwner1.account.address, coinSpender.account.address]);
+
+		assert.equal(balanceOwner2, balanceOwner1 - amount);
+		assert.equal(balanceReceiver2, balanceReceiver1 + amount);
+		assert.equal(allowanceSpender2, allowanceSpender1 - amount);
+
+		const events = await client.getContractEvents({
+			abi: cERC20Token.abi,
+			eventName: 'Transfer',
+			fromBlock: blockN,
+		});
+
+		assert.equal(events.length, 1);
+		assert.deepEqual(events[0].args, {
+			from: getAddress(coinOwner1.account.address),
+			to: getAddress(coinReceiver.account.address),
+			amount,
+		});
+	});
+
+	it("transferFrom() over 9000 - revert InsufficientAllowance", async function () {
+		const amount = 9001n;
+
+		const spenderAllowance = await cERC20Token.read.allowance([coinOwner1.account.address, coinSpender.account.address]);
+
+		try {
+			await cERC20Token.write.transferFrom(
+				[coinOwner1.account.address, coinReceiver.account.address, amount],
+				{account: coinSpender.account},
+			);
+			assert.fail(`Contract should have failed.`);
+		} catch (e: any) {
+			assert.equal(
+				e.details,
+				testutils.contractErrorMessage(
+					'InsufficientAllowance',
+					coinOwner1.account.address,
+					coinSpender.account.address,
+					spenderAllowance,
+					amount
+				)
+			);
+		}
+	});
+
+	it("transferFrom() from address(0) - revert InvalidSender", async function () {
+		const amount = 2n;
+
+		try {
+			await cERC20Token.write.transferFrom(
+				[addr0, coinReceiver.account.address, amount],
+				{account: coinSpender.account},
+			);
+			assert.fail(`Contract should have failed.`);
+		} catch (e: any) {
+			assert.equal(e.details, testutils.contractErrorMessage('InvalidSender', addr0));
+		}
+	});
+});
